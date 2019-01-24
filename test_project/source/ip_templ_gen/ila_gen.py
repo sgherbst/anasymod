b8d8b90ae0adf4ad5d20c6e1a2235d7c73ffed38
Gabriel Rutsch <gabriel.rutsch@infineon.com>
2019-01-24 11:25:40 +0100

    UPDATE ila generator

--- test_project/source/ip_templ_gen/ila_gen.py
+++ test_project/source/ip_templ_gen/ila_gen.py
@@ -1,34 +1,30 @@
 from os.path import dirname, join, exists, isfile
 from os import makedirs
 from argparse import ArgumentParser
-
-from msdsl.files import get_full_path, get_dir
-from msdsl.util import call_python
-
-from ast import literal_eval
-
 from ip_core_gen.ip_core_gen import Clk_wiz_gen, Vio_gen, ILA_gen
 
 def main():
     # parse command line arguments
     parser = ArgumentParser()
-
-    parser.add_argument('-i', '--input', type=str, default=get_dir(join('source' 'constraints')))
-    parser.add_argument('-o', '--output', type=str, default=get_dir(join('build' 'constraints')))
-    parser.add_argument('-r', '--root', type=str, default=get_dir())
-    parser.add_argument('-c', '--configfile', type=str, default=r"C:\Inicio_dev\test_project\build\test_project\probe_config.txt")
+    parser.add_argument('-r', '--root', type=str, default=r"C:\Inicio_dev\anasymod\test_project")
+    parser.add_argument('-c', '--configfile', type=str, default=r"C:\Inicio_dev\anasymod\test_project\build\test_project\probe_config.txt")
     parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args()
 
     # Read Probe Config file
     with (open(args.configfile, "r")) as cf:
-        cfg_dict = literal_eval(cf.read())
+        config = cf.readlines()
 
+    cfg_dict = {}
+    cfg_dict['sb'] = config[0].strip(r"SB:").strip().split()
+    cfg_dict['mb'] = config[1].strip(r"MB:").strip().split()
+    cfg_dict['mb_exponent'] = config[2].strip(r"MB_EXPONENT:").strip().split()
+    cfg_dict['mb_width'] = config[3].strip(r"MB_WIDTH:").strip().split()
 
 
     # Specify templates to be generated
-    ila_0 = ILA_gen(project_root=args.root, ip_name=r"ila_0", constr_name=r"constr.xdc")
+    ila_0 = ILA_gen(project_root=args.root, ip_name=r"ila_0", constr_name=r"constr.xdc", inst_name=r"u_ila_0")
 
     count = 0
 
@@ -40,7 +36,7 @@ def main():
         count += 1
 
     # Add multi bit probes
-    for conn, width in cfg_dict['ms']:
+    for conn, width in zip(cfg_dict['mb'], cfg_dict['mb_width']):
         ila_0.subst_dict['probes']['probe{0}'.format(count)] = {}
         ila_0.subst_dict['probes']['probe{0}'.format(count)]['conn'] = r"{0}".format(conn)
         ila_0.subst_dict['probes']['probe{0}'.format(count)]['width'] = r"{0}".format(width)
