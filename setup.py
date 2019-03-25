71b6e838885cf21fcd6e4a10ca0b4391c5127d63
sgherbst <sgherbst@users.noreply.github.com>
2019-03-25 18:34:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='anasymod',
     version='0.0.1',
     description='Vivado automation flow for running mixed-signal emulations on FPGAs',
-    url='?????????????????????????????????',
+    url='https://github.com/sgherbst/anasymod.git',
     author='Gabriel Rutsch, Steven Herbst, Shivani Saravanan',
     author_email='gabriel.rutsch@infineon.com, sherbst@stanford.edu, shivani.saravanan@infineon.com',
     packages=['anasymod'],
