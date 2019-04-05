cbd430e900481809c5eb1f850b22210739700c12
Steven Herbst <sherbst@stanford.edu>
2019-04-05 16:35:59 -0700

    adding trenz board

--- anasymod/fpga_boards/boards.py
+++ anasymod/fpga_boards/boards.py
@@ -20,6 +20,17 @@ class PYNQ_Z1():
         'short_part_name' : 'xc7z020'
         }
 
+class TE0720():
+    def __init__(self):
+        self.cfg = {
+        'clk_pin_p' : 'F7',
+        'clk_pin_n' : None,
+        'clk_io' : 'LVCMOS33',
+        'clk_freq' : 33.333333e6,
+        'full_part_name' : 'xc7z020clg484-1',
+        'short_part_name' : 'xc7z020'
+        }
+
 class VC707():
     def __init__(self):
         self.cfg = {
