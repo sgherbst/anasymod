353670cb8c9ad67675d3c37cc3e834833b9fa0da
Steven Herbst <sherbst@stanford.edu>
2020-08-04 18:29:51 -0700

    try re-running regression with FPGA-based tests included

--- regress.sh
+++ regress.sh
@@ -15,12 +15,25 @@ export PYTHON_MSDSL=`which python`
 echo $PYTHON_MSDSL
 
 # use a default target if not specified
-if [ -z "$ANASYMOD_TARGET" ]; then ANASYMOD_TARGET=sim_icarus; fi
+if [[ -z "$ANASYMOD_TARGET" ]]; then
+    ANASYMOD_TARGET=sim_icarus
+fi
 
-# run tests.  optional arguments:
+# run simulation-based tests.  optional arguments:
 # --target <target_name> to specify the simulator (sim_icarus, sim_vivado, etc.)
 # --classification <classification> to select which tests are run (basic, weekend, etc.)
-pytest --cov-report=xml --cov=anasymod --target=$ANASYMOD_TARGET unittests -v -r s -s
+pytest --cov-report=xml -v -r s -s \
+    --cov=anasymod --target=$ANASYMOD_TARGET unittests
+
+# if we're on the FPGA server, then run some tests on the FPGA board
+# note that --cov-append is used so that we added to the previously
+# collected coverage data
+if [[ -n "${FPGA_SERVER}" ]]; then
+    pytest --cov-report=xml --cov-append -v -r s -s \
+        --cov=anasymod --target=emulate_vivado \
+        unittests/basic_sim/test_basic_sim.py::TestBasicSIM.test_firmware \
+        unittests/basic_sim/test_basic_sim.py::TestBasicSIM.test_rc
+fi
 
 # upload coverage information
 bash <(curl -s https://codecov.io/bash)
