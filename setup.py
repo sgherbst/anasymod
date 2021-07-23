c176e9503fcd480283ded2c3c35bfb2c87ce8839
Steven Herbst <sgherbst@users.noreply.github.com>
2021-07-23 16:57:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = 'anasymod'
-version = '0.4.0.dev6'
+version = '0.4.0'
 
 DESCRIPTION = '''\
 Tool for running mixed-signal emulations on FPGAs\
